5167d6aed1cae831f52b53918edf29b8bf880a6b
prodevweb21 <prodevweb21@gmail.com>
2021-09-17 01:47:09 -0400

    Bouton installer

---
+++ projet_final_pwa/js/install.js
@@ -0,0 +1,36 @@
+    let deferredInstallPrompt = null;
+    const installButton = document.getElementById('butInstall');
+    installButton.addEventListener('click', installPWA);
+    // Add event listener for beforeinstallprompt event
+    window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+    /**
+    * Event handler for beforeinstallprompt event.
+    * Saves the event & shows install button.
+    *
+    * @param {Event} evt
+    */
+    function saveBeforeInstallPromptEvent(evt) {
+    //CODELAB: Add code to save event & show the install button
+    deferredInstallPrompt = evt;
+    installButton.removeAttribute('hidden');
+    }
+    /**
+    * Event handler for butInstall - Does the PWA installation.
+    *
+    * @param {Event} evt
+    */
+    function installPWA(evt) {
+        // CODELAB: Add code show install prompt & hide the install button.
+        // CODELAB: Log user response to prompt.
+    }
+    // CODELAB: Add event listener for appinstalled event
+    /**
+     * Event handler for appinstalled event.
+     * Log the installation to analytics or save the event somehow.
+     *
+     * @param {Event} evt
+     */
+    function logAppInstalled(evt) {
+        // CODELAB: Add code to log the event
+    }
+    
