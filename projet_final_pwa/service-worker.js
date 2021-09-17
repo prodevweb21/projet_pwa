d218970faf96d7ce922558f2f23bf13d9770c2ff
prodevweb21 <prodevweb21@gmail.com>
2021-09-17 00:24:11 -0400

    .

--- projet_final_pwa/service-worker.js
+++ projet_final_pwa/service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-cache-v2';
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
     "index.html",
@@ -77,6 +77,17 @@ const FILES_TO_CACHE = [
         self.addEventListener('activate', (evt) => {
         console.log('[ServiceWorker] Activate');
         //Remove previous cached data from disk.
+        evt.waitUntil(
+            caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+            if (key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache',
+           key);
+            return caches.delete(key);
+            }
+            }));
+            })
+            );
         self.clients.claim();
         });
         self.addEventListener('fetch', (evt) => {
