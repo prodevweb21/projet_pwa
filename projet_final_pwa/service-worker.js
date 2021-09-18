58f4ebd6a44ea6570b7b3765344fde2902246102
prodevweb21 <prodevweb21@gmail.com>
2021-09-17 21:39:05 -0400

    .

--- projet_final_pwa/service-worker.js
+++ projet_final_pwa/service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v22';
+const CACHE_NAME = 'static-cache-v23';
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
     "offline.html",
